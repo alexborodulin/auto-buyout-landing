e678c263424b5d43bcc6d0aba4301b86f7593e90
Alex Borodulin <sasha7818@yandex.ru>
2026-07-17 02:42:50 +0300

    [master] Добавил метрику

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,6 +55,21 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       ],
+      script: [
+        {
+          key: 'yandex-metrika',
+          type: 'text/javascript',
+          innerHTML: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110802910', 'ym');
+ym(110802910, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+`.replace(/\n/g, ''),
+        },
+      ],
       noscript: [
         {
           children:
